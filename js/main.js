5aea4a0d09627bc0a04632ae299f27b4bd6edb58
inferno-steel <inferno-steel@mail.ru>
2024-08-28 16:50:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+
+
+document.querySelector(".burger-menu").addEventListener("click", () => {
+    document.querySelector(".header-wrap").classList.toggle("burger-open")
+})
